4c6f8aaa9284df9ce2bb345f489e192827df09cc
Orion00002 <133288698+Orion00002@users.noreply.github.com>
2023-05-15 22:53:49 -0600

    Update SpotifyRat.py

--- src/SpotifyRat.py
+++ src/SpotifyRat.py
@@ -6,19 +6,18 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post, get
+import json
 
 load_dotenv()
 
 spot_client_id = os.getenv("SPOT_CLIENT_ID")
 spot_client_secret = os.getenv("SPOT_CLIENT_SECRET")
 
-print(spot_client_id, spot_client_secret)
-
 def get_token():
     auth_string = spot_client_id + ":" + spot_client_secret
     auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
     url = "https://accounts.spotify.com/api/token"
     headers = {
@@ -31,5 +30,34 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists.")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
 token = get_token()
-print(token)
+result = search_for_artist(token, "blackpink")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
