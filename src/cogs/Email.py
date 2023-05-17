641fa075a3a74f94310b91d8e1e9a1537e372ffc
Gavin Castaneda <gcastaneda456@gmail.com>
2023-05-17 17:59:51 -0600

    Implement a few methods #5
    Implement checkemails
    Implement printemails
    Implement check_email
    Implement print_emails
    Not completely working yet
    Also formatted it so the cog loads.

--- src/cogs/Email.py
+++ src/cogs/Email.py
@@ -11,18 +11,62 @@ Copyright 2023 Gavin Castaneda
 
 # ==== Includes ====
 from discord.ext import tasks, commands
-import Email
+from Email import Email as EmailClass
 
 # ==== Class ====
 class Email(commands.Cog):
     # ==== Init ====
-    def __init__(self):
-        pass
+    def __init__(self, bot):
+        self.bot = bot
+        self.mailbox = EmailClass()
+
+    
+    # ==== Methods ====
+    async def print_emails(self, context):
+        print(f'- [Email] print_emails called.')
+        if self.parsed_emails is not None:
+            for email in self.parsed_emails:
+                await context.send(f"Author: {email['author']}")
+                await context.send(f"Author: {email['subject']}")
+        else:
+            print('Else')
+            await context.send(f"No emails found.")
 
 
     # ==== Tasks ====
     @tasks.loop(minutes=5.0)
     async def check_email(self):
         """Checks email every 5 minutes"""
-        pass
-        # TODO: Check email stuff here
+        #TODO: logging levels
+        print(f'- [Email] check_email called.')
+
+        # Get unread emails
+        self.emails = await self.mailbox.get_emails()
+        self.parsed_emails = await self.mailbox.parse_emails(self.emails)
+
+    # ==== Commands ====
+    @commands.command(name='checkemails',
+                help='Forces bot to check emails',
+                brief='Forces bot to check emails')
+    async def checkemails(self, context):
+        author = context.message.author
+        print(f'- [Email] !checkemails called by {author}')
+        await context.send("Checking emails...")
+        await self.check_email()
+    
+
+    @commands.command(name='printemails',
+                help='Forces bot to print emails',
+                brief='Forces bot to print emails')
+    async def printemails(self, context):
+        author = context.message.author
+        print(f'- [Email] !printemails called by {author}')
+        await context.send("Printing emails...")
+        await self.print_emails(context)
+    
+
+
+
+# ==== Setup ====
+async def setup(bot):
+    await bot.add_cog(Email(bot))
