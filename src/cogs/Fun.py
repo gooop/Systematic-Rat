8bd5622b2015eb52ace66d35b4de993e65a62713
Gavin Castaneda <gcastaneda456@gmail.com>
2025-11-11 13:10:35 -0700

    remove that cursed command

--- src/cogs/Fun.py
+++ src/cogs/Fun.py
@@ -41,40 +41,6 @@ class Fun(commands.Cog):
             image_url = "https://media.tenor.com/aaEMtGfZFbkAAAAi/rat-spinning.gif'"
             await context.send(image_url)
 
-
-    @commands.command(name='ratcum',
-                    hidden=True,
-                    brief='What will you get??')
-    async def ratcum(self, context):
-        # Delete command from user and log
-        author = context.message.author
-        print(f'- [Fun] !ratcum called by {author}')
-        author = str(author).split('#')[0]
-        await context.message.delete()
-
-        # Try to upload, if that fails, paste URL
-        try:
-            rand_int = random.randint(0, 100)
-            if rand_int <= 50:
-                with open('../assets/Top_Rat.jpg', 'rb') as f:
-                    picture = discord.File(f)
-                    picture.filename = f'SPOILER_{picture.filename}'
-                    await context.send(f'{author} says: ~uuhhnnh~', file=picture)
-            if rand_int > 50 and rand_int <= 99:
-                with open('../assets/Bottom_Rat.jpg', 'rb') as f:
-                    picture = discord.File(f)
-                    picture.filename = f'SPOILER_{picture.filename}'
-                    await context.send(f'{author} says: ~uuhhnnh~', file=picture)
-            if rand_int == 100:
-                with open('../assets/Chad_Rat.jpg', 'rb') as f:
-                    picture = discord.File(f)
-                    picture.filename = f'SPOILER_{picture.filename}'
-                    await context.send(f'{author} **ROLLED A RARE RAT!**', file=picture)
-
-        except:
-                await context.send('Error: Rat could not finish')
-
-
 # ==== Setup ====
 async def setup(bot):
     await bot.add_cog(Fun(bot))
